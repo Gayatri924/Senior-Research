4646170a3da84a913d842f8e9168c5eb37fac9c0
Gayatri Gopavajhala <gayatri924@gmail.com>
2020-11-19 16:51:25 -0500

    adding tasks works

--- app/src/main/java/com/gopavajhalagayatri/seniorresearch/MainActivity.java
+++ app/src/main/java/com/gopavajhalagayatri/seniorresearch/MainActivity.java
@@ -17,17 +17,26 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private String m_Text = "";
+    ListView simpleList;
+    ArrayList<String> taskList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        simpleList = (ListView)findViewById(R.id.list_view);
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.tasks_list, R.id.taskItem, taskList);
+        simpleList.setAdapter(arrayAdapter);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -43,7 +52,8 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         m_Text = input.getText().toString();
-                        Log.i("task", m_Text);
+                        taskList.add(m_Text);
+                        arrayAdapter.notifyDataSetChanged();
                     }
                 });
                 builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
